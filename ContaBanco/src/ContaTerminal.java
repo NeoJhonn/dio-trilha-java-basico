bc01247404f7507b680b622361c3d36fb12791e1
NeoJhonn <jhonny.azevedo@gmail.com>
2024-05-04 23:43:41 -0300

    Adicionado fechamento do scanner

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -23,6 +23,9 @@ public class ContaTerminal {
         if (tec.hasNextDouble())
             saldo = tec.nextDouble();
 
+        // fechar o Scanner
+        tec.close();
+
         // Imprimir dados da conta no terminal
         System.out.println("Olá " +nomeCliente+ ", obrigado por criar uma conta em nosso banco, sua agência é " +
                            agencia+ ", conta " +numero+ " e seu saldo " +saldo+ " já está disponível para saque.");
